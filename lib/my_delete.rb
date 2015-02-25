60cc93664adca1f4be6319fe8988716bcde5c42c
Katie Hoffman <ktahoffman@gmail.com>
2015-02-25 01:06:04 -0500

    update to include answer

--- lib/my_delete.rb
+++ lib/my_delete.rb
@@ -1,1 +1,25 @@
-# code here
+class Array
+
+  def delete_first(item)
+    self.delete_at(self.index(item))
+    self
+  end
+
+  def delete_last(item)
+    self.delete_at(self.rindex(item))
+    self
+  end
+
+  def delete_middle(item)
+    indexes = self.map.with_index {|element, i| i if element == item }.compact
+    length = indexes.length
+    halfway = length / 2
+    if length.odd?
+      self.delete_at(indexes[halfway])
+    else
+      [halfway, halfway - 1].each {|i| self.delete_at(indexes[i]) }
+    end
+    self
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require_relative "../lib/my_delete.rb"
-
 RSpec.configure do |config|
   # config here
 end
